ad076d518a74d8862eeba9d29e004702d6861f0a
Ivan <icabulo@gmail.com>
2023-01-31 14:23:40 -0500

    second test for 100% coverage

--- src/robot.test.js
+++ src/robot.test.js
@@ -1,12 +1,20 @@
 const { createRobot } = require("./robot");
 
-describe("Test path RAALAL", () => {
-  it("Should output the final position coordinates and orientation", () => {
+describe("Test two paths", () => {
+  it("Should output the final position for path RAALAL", () => {
     const myRobot = createRobot(7, 3, "N");
     myRobot.instruction("RAALAL");
     expect(myRobot.getPosition()).toMatchObject({
       position: [9, 4],
       orientation: "W",
+    });
+  });
+  it("Should output the final position for path ARAARARALAALLLRRRA", () => {
+    const myRobot = createRobot(7, 3, "N");
+    myRobot.instruction("ARAARARALAALLLRRRA");
+    expect(myRobot.getPosition()).toMatchObject({
+      position: [8, 0],
+      orientation: "S",
     });
   });
 });
